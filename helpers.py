d58bbd5e7dbf8d2706afb834e3d642666615a0ec
Syed Fasih Uddin <you@example.com>
2024-01-16 16:07:34 +0500

    initial code

---
+++ helpers.py
@@ -0,0 +1,35 @@
+import re
+import pandas as pd
+
+def segregate_messages(df):
+    users = []
+    messages = []
+    for message in df['user_message']:
+        entry = re.split('([\w\W]+?):\s', message)
+        if entry[1:]:  # username exists
+            users.append(re.split(' - ', entry[1])[1])
+            messages.append(" ".join(entry[2:]))
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+    return users,messages
+
+def abc():
+    pattern = '\d{1,2}\/\d{1,2}\/\d{2},\s\d{1,2}:\d{2}\s(?:AM|PM)'
+    messages = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+    df = pd.DataFrame({'user_message': messages, 'date': dates})
+    df['date'] = pd.to_datetime(df['date'], format='%m/%d/%y, %I:%M %p')
+
+    df['user'] , df['message'] = segregate_messages(df)
+    df.drop(columns=['user_message'], inplace=True)
+    df['only_date'] = df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+
+    return df
